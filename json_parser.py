22ade8895a7eb5767faed24682921c8853abdb33
Alex Ray <1alexray@gmail.com>
2017-10-21 18:56:09 -0600

    Fixed error raising structure

--- json_parser.py
+++ json_parser.py
@@ -8,13 +8,13 @@ class Json_Parser:
         self.parsed_lines = []
 
     def __timestamp_to_string(self, date, time):
-        if (not date.isdigit() or len(date) != 8)
-            raise RuntimeError("Invalid date")
-        elif (not time.isdigit() or len(time) != 6)
-            raise RuntimeError("Invalid time")
+        if (not date.isdigit() or len(date) != 8):
+            raise RuntimeError("Error: Invalid date")
+        elif (not time.isdigit() or len(time) != 6):
+            raise RuntimeError("Error: Invalid time")
 
-        return "%s-%s-%s %s:%s:%s" %
-            (date[0:4], date[4:6], date[6,8], time[0:2], time[2:4], time[4:6])
+        ret = "%s-%s-%s %s:%s:%s" % (date[0:4], date[4:6], date[6:8], time[0:2], time[2:4], time[4:6])
+        return ret
 
     def parse_file(self, filename, levels):
         f = open(filename, 'r')
@@ -24,23 +24,27 @@ class Json_Parser:
             values = []
 
             # Removing punctuation
-            line = re.sub('[]', '', line)
-            line = re.sub(':', ' ', line)
+            line = line.replace('[', '')
+            line = line.replace(']', '')
+            line = line.replace(':', ' ')
 
-            fields = line.split(' ') # String to list
+            fields = line.split() # String to list
 
-            if (fields[0] not in levels) continue else values.append(fields[0])
+            if (fields[0] not in levels):
+                continue
+            else:
+                values.append(fields[0])
 
             try:
                 values.append(self.__timestamp_to_string(fields[1], fields[2]))
 
-                tmp = int(fields[3])
+                tmp = int(fields[4])
                 if (tmp <= 0):
-                    raise RuntimeError("Invalid line number")
+                    raise RuntimeError("Error: Invalid line number")
                 values.append(fields[3])
             except Exception as e:
                 raise
 
-            values.append(' '.join(fields[4:]))
+            values.append(' '.join(fields[5:]))
 
             self.parsed_lines.append(OrderedDict(zip(self.keys, values)))
--- log_to_json.py
+++ log_to_json.py
@@ -7,29 +7,40 @@ def main():
     levels = ["TRACE", "DEBUG", "INFO", "WARN", "ERROR"]
     keys = ["level", "timestamp", "file", "line", "message"]
 
+    default_level = True
+    file_included = False
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'f:ld', [])
-    except getopt.GetoptError as e:
+        opts, args = getopt.getopt(sys.argv[1:], 'f:l:d:')
+        for o, a in opts:
+            if o == "-f":
+                file_included = True
+                filename = a
+            if o == "-l":
+                default_level = False
+                try:
+                    levels = levels[levels.index(a.upper()):]
+                except ValueError:
+                    raise ValueError("Error: Invalid level specifier")
+        if (not file_included):
+            raise RuntimeError("Error: filename argument required")
+    except Exception as e:
         print(e)
         sys.exit(2)
 
-    default_level = True
-    for o, a in opts:
-        if o == "-f":
-            filename = a
-        if o == "-l":
-            default_level = False
-            try:
-                levels = levels[levels.index(a.upper()):]
-            except ValueError("Invalid level specifier")
-
-    if (default_level) levels = levels[levels.index("INFO"):]
+    if (default_level):
+        levels = levels[levels.index("INFO"):]
 
     jp = json_parser.Json_Parser(keys)
-    dicts = jp.parse_file(filename, levels)
-
-    for d in dicts:
-        json.dumps(d)
+    try:
+        jp.parse_file(filename, levels)
+        for d in jp.parsed_lines:
+            print(json.dumps(d))
+    except Exception as e:
+        for d in jp.parsed_lines:
+            print(json.dumps(d))
+        print(e)
+        sys.exit(2)
 
 if __name__ == '__main__':
     main()
